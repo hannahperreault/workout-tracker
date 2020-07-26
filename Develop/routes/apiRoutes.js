b41b7cc0401b3176d4725eb9d5c86782bd29fe52
Hannah Perreault <hperre@gmail.com>
2020-07-26 10:51:50 -0400

    deploying herkou

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const Workout = require('../models/Workout');
+
+
+//example. make sure routes match how its set up here
+
+router.post("/api/workout", (reg, res)=> {
+    Workout.create({}).then ()
+});
