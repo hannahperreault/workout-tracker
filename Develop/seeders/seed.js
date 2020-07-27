aa5b528447bde8d160c9c4e65a2ade69a6cbd4dc
Hannah Perreault <hperre@gmail.com>
2020-07-26 20:15:56 -0400

    still trying heroku

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,13 +1,7 @@
 let mongoose = require('mongoose');
 const db = require('../models/Workout');
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env
-  );
-});
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts', {
   useNewUrlParser: true,
