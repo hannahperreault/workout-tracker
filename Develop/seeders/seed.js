36fe2ab58d164337cf9c2ccb74410f0d7f7e3a72
Hannah Perreault <hperre@gmail.com>
2020-07-26 20:11:17 -0400

    updated port info

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,7 +1,13 @@
 let mongoose = require('mongoose');
 const db = require('../models/Workout');
 
-const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts', {
   useNewUrlParser: true,
